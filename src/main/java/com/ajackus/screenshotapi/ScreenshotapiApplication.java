2310cc02ce519f40cd6512ce4365ce38042fcaa7
PreethiBalakrishnan <preethib370@gmail.com>
2021-10-24 14:39:57 +0530

    Initial Commit

---
+++ src/main/java/com/ajackus/screenshotapi/ScreenshotapiApplication.java
@@ -0,0 +1,13 @@
+package com.ajackus.screenshotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenshotapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenshotapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajackus/screenshotapi/ScreenshotapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajackus.screenshotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenshotapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
